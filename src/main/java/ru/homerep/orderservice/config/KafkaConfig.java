677e3db3a401b71273e1b30a035c562368307618
Siderea <hutornoyaa@gmail.com>
2025-05-25 19:59:14 +0300

    fix kafka conf

--- src/main/java/ru/homerep/orderservice/config/KafkaConfig.java
+++ src/main/java/ru/homerep/orderservice/config/KafkaConfig.java
@@ -12,12 +12,52 @@ import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.*;
+import ru.homerep.orderservice.models.dto.OrderRequest;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
 public class KafkaConfig {
+    @Bean
+    public ProducerFactory<String, Order> orderProducerFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, org.springframework.kafka.support.serializer.JsonSerializer.class);
+        return new DefaultKafkaProducerFactory<>(configProps);
+    }
+
+    @Bean
+    public KafkaTemplate<String, Order> kafkaTemplateOrder() {
+        return new KafkaTemplate<>(orderProducerFactory());
+    }
+
+    @Bean
+    public ProducerFactory<String, OrderRequest> orderRequestProducerFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, org.springframework.kafka.support.serializer.JsonSerializer.class);
+        return new DefaultKafkaProducerFactory<>(configProps);
+    }
+
+    @Bean
+    public KafkaTemplate<String, OrderRequest> kafkaTemplateOrderRequest() {
+        return new KafkaTemplate<>(orderRequestProducerFactory());
+    }
+
+    @Bean
+    public ProducerFactory<String, String> stringProducerFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        return new DefaultKafkaProducerFactory<>(configProps);
+    }
+
+    @Bean
+    public KafkaTemplate<String, String> kafkaTemplateString() {
+        return new KafkaTemplate<>(stringProducerFactory());
+    }
+
 
     @Bean
     public NewTopic orderTopic() {
