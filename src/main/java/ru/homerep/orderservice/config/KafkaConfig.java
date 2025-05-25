36b3f4905b80327053d927b6a8bd814a14559634
Siderea <hutornoyaa@gmail.com>
2025-05-25 22:10:54 +0300

    pls kafka just work

--- src/main/java/ru/homerep/orderservice/config/KafkaConfig.java
+++ src/main/java/ru/homerep/orderservice/config/KafkaConfig.java
@@ -20,12 +20,32 @@ import java.util.Map;
 @Configuration
 public class KafkaConfig {
     @Bean
+    public ProducerFactory<String, String> producerFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(
+                ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                "kafka1:19092,kafka2:19092");
+        configProps.put(
+                ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+                StringSerializer.class);
+        configProps.put(
+                ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+                StringSerializer.class);
+        return new DefaultKafkaProducerFactory<>(configProps);
+    }
+
+    @Bean
+    public KafkaTemplate<String, String> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+
+    @Bean
     public ProducerFactory<String, Order> orderProducerFactory() {
         Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka1:19092,kafka2:19093");
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, org.springframework.kafka.support.serializer.JsonSerializer.class);
 
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka1:19092,kafka2:19093");
         return new DefaultKafkaProducerFactory<>(configProps);
     }
 
@@ -37,9 +57,9 @@ public class KafkaConfig {
     @Bean
     public ProducerFactory<String, OrderRequest> orderRequestProducerFactory() {
         Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka1:19092,kafka2:19093");
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, org.springframework.kafka.support.serializer.JsonSerializer.class);
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka1:19092,kafka2:19093");
         return new DefaultKafkaProducerFactory<>(configProps);
     }
 
@@ -51,9 +71,9 @@ public class KafkaConfig {
     @Bean
     public ProducerFactory<String, String> stringProducerFactory() {
         Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka1:19092,kafka2:19093");
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka1:19092,kafka2:19093");
 
         return new DefaultKafkaProducerFactory<>(configProps);
     }
@@ -91,38 +111,30 @@ public class KafkaConfig {
         return new NewTopic("master-found-topic", 1, (short) 1);
     }
 
-        @Bean
-        public ProducerFactory<String, String> producerFactory() {
-            Map<String, Object> configProps = new HashMap<>();
-            // ваши настройки producer
-            configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-            configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-            return new DefaultKafkaProducerFactory<>(configProps);
-        }
 
-        @Bean
-        public KafkaTemplate<String, String> kafkaTemplate() {
-            return new KafkaTemplate<>(producerFactory());
-        }
-
-        @Bean
-        public ConsumerFactory<String, String> consumerFactory() {
-            Map<String, Object> props = new HashMap<>();
-            props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-            props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-            return new DefaultKafkaConsumerFactory<>(props);
-        }
-
-        @Bean
-        public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
-            ConcurrentKafkaListenerContainerFactory<String, String> factory =
-                    new ConcurrentKafkaListenerContainerFactory<>();
-            factory.setConsumerFactory(consumerFactory());
-            return factory;
-        }
+    @Bean
+    public ConsumerFactory<String, String> consumerFactory() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(
+                ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                "kafka1:19092,kafka2:19092");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        return new DefaultKafkaConsumerFactory<>(props);
+    }
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, String> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        return factory;
+    }
     @Bean
     public ConsumerFactory<String, Order> orderConsumerFactory() {
         Map<String, Object> props = new HashMap<>();
+        props.put(
+                ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                "kafka1:19092,kafka2:19092");
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
         props.put(JsonDeserializer.TRUSTED_PACKAGES, "*"); // or restrict to your package
